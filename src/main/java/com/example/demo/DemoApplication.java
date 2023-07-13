b1410db76264fff8460d6fa32712727138ed2354
TusharHajare <tusharhajare7768@gmail.com>
2023-07-13 10:54:42 +0530

    new information was updated

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,12 +6,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		SpringApplication.run(DemoApplication.class, args);
 
 		System.out.println("manoj");
 		System.out.println("manoj2");
 		System.out.println("tinapatil");
+		System.out.println("Tushar Hajare");
 	}
 
 
